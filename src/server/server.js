b607fe9d76445691bd554633847fcefc6901adbb
jonharrity <joharrity@gmail.com>
2017-10-20 07:20:45 -0400

    added functionality to exchange code for access_token

--- src/server/server.js
+++ src/server/server.js
@@ -24,10 +24,28 @@ app.get('/cb', (req,res) => {
 	var store = app.get('states');
 	var state = req.query.state;
 	var code = req.query.code;
-	if( typeof state === 'string' && typeof code === 'string' && (typeof store[state] != 'undefined') ) {
-		store[state].code = code;
+	var id = req.query.user_id;
+	if( typeof state === 'string' && typeof code === 'string' && typeof id === 'string' && (typeof store[state] != 'undefined') ) {
+		store[state].id = id;
 		app.set('states',store);
 		res.send('success');
+		var xhr = new XMLHttpRequest();
+		var basic = new Buffer('22CJHV:5b010441676c31c691685973128defb0').toString('base64');
+		var params = 'client_id=22CJHV&grant_type=authorization_code&redirect_uri=https://desolate-river-72208.herokuapp.com'+req.originalUrl;
+		xhr.open('POST','https://api.fitbit.com/oauth2/token',true);
+		xhr.setRequestHeader('Authorization','Basic ' + basic);
+		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+		xhr.send(params);
+		xhr.onreadystatechange = function(v) { 
+			return () => {
+				var n = v.n;
+				if( n.readyState === XMLHttpRequest.DONE && n.status === 200 ) {
+					var store = app.get('states');
+					var state = v.state;
+					store[state].token = JSON.parse(n.responseText).access_token;
+				}
+			};
+		}({n: xhr, state: state});
 	}
 	else
 		res.send('error');
@@ -35,8 +53,10 @@ app.get('/cb', (req,res) => {
 app.get('/pop', (req,res) => {
 	var store = app.get('states');
 	var state = req.query.state;
-	if( typeof state === 'string' && (typeof store[state] != 'undefined')) {
-		res.send(store[state].code);
+	if( typeof state === 'string' && (typeof store[state] != 'undefined') && (typeof store[state].id != 'undefined') && (typeof store[state].token != 'undefined') ) {
+		var obj = { 'token': store[state].token,
+					'id': store[state].id};
+		res.send(JSON.stringify(obj));
 	}
 	res.send('');
 });
