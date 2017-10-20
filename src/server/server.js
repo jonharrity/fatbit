e1a6019bee574ced1e3b2e53806980529e6cd5f4
jonharrity <joharrity@gmail.com>
2017-10-20 03:35:04 -0400

    Added pseudocode for processing server requests: push, pop, cb

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,31 @@
 var express = require('express');
 var app = express();
 var port = (process.env.PORT || 3000);
+var maxStateStorageTime = 1000 * 60 * 5;
+
+app.set('states', {});
+
 app.get('/', (req,res) => res.sendFile(__dirname+'/index.html'));
+app.get('/cb', (req,res) => {
+	var store = app.get('states');
+//	var code = red . something
+//	var state = req . something
+//	store[state].code = code;
+});
+app.get('/push', (req,res) => {
+	var store = app.get('states');
+//	var state = req.something
+//	states[ state ] = {time: timestamp}
+	setTimeout(() => {
+		var store = app.get('states');
+		delete store[state];
+	}, maxStateStorageTime, 'put the state here');
+});
+app.get('/pop', (req,res) => {
+	var store = app.get('states');
+//	var state = req. something;
+//	var code = store[state].code; --put in try or something
+//	res.send( the access token ));
+	res.send('nothing yet :/');
+});
 app.listen(port,()=>console.log('fatbit server started'));
